202dbac8ae5b91214049613d4061fd4f833a5c3b
ilyaskleine <ilyaskleine07@gmail.com>
2024-04-10 00:36:41 +0200

    Raten, Score und meherer Runden

--- simon-says.py
+++ simon-says.py
@@ -1,6 +1,10 @@
 import pygame
 import random
-import time
+
+red = (255, 0, 0)
+green = (0, 255, 0)
+blue = (0, 0, 255)
+yellow = (255, 255, 0)
 
 # Klasse für Verarbeitung von Tastatur / Sensoren
 class GameInput:
@@ -18,11 +22,6 @@ class GameInput:
                 return "left"
             if keys[pygame.K_w]:
                 return "right"
-        else:
-            with open("herald.lag", "r") as inputfile:
-                print("File: " + inputfile.readline())
-                #print(time.time())
-            
             
 class User:
     def __init__(self):
@@ -37,51 +36,93 @@ class Field:
     def __init__(self, side):
         global screen
         self.side = side
+        # Computed values
         self.posX = screen.get_width() / 2 - side / 2
         self.posY = screen.get_height() / 2 - side / 2
-        self.colorsToKey = {"red": "up", "violet": "down", "blue": "left", "green": "right"}
-        #self.color = random.choice(list(self.colorsToKey.keys()))
-        self.color = "black"
         cornerWidth = side / 2
-
-        self.topLeft = Corner(self.posX, self.posY, cornerWidth, "blue")
-        self.topRight = Corner(self.posX + cornerWidth, self.posY, cornerWidth, "red")
-        self.bottomLeft = Corner(self.posX, self.posY  + cornerWidth, cornerWidth, "green")
-        self.bottomRight = Corner(self.posX + cornerWidth, self.posY + cornerWidth, cornerWidth, "violet")
-
+        # Config 
+        self.colorsToKey = {"red": "right", "yellow": "down", "blue": "up", "green": "left"}
+        self.color = "black"
+        self.blinkTime = 1000
+        # (Child) Objects
+        self.topLeft = Corner(self.posX, self.posY, cornerWidth, blue)
+        self.topRight = Corner(self.posX + cornerWidth, self.posY, cornerWidth, red)
+        self.bottomLeft = Corner(self.posX, self.posY  + cornerWidth, cornerWidth, green)
+        self.bottomRight = Corner(self.posX + cornerWidth, self.posY + cornerWidth, cornerWidth, yellow)
+        # Algoritmic Variables
         self.query = []
+        self.queryLength = 1
+        self.setRandomQuery()
         self.index = 0
         self.round = 0
-        self.blinkTime = 900
         self.activationTick = pygame.time.get_ticks()
-        self.setRandomQuery(5)
-
+        self.guess = None
+        self.guessIndex = 0
+        self.cooldown = pygame.time.get_ticks()
+        self.score = 0
 
     def draw(self):
         global screen
         self.act()
+        # Feld + Subdivs
         pygame.draw.rect(screen, self.color, pygame.Rect(self.posX, self.posY, self.side, self.side))
         self.topLeft.draw()
         self.topRight.draw()
         self.bottomLeft.draw()
         self.bottomRight.draw()
+        # Score
+        font = pygame.font.Font(None, 36)
+        score_text = font.render(f'Score: {self.score}', True, (0, 0, 0))
+        screen.blit(score_text, (10, 10))
 
     def act(self):
-        if len(self.query) == self.index:
-            # Finished Show-Off Phase
-            return
         now = pygame.time.get_ticks()
-        if now - self.activationTick >= self.blinkTime:
+        if len(self.query) == self.index: # Wenn Alle gezeigt wurden ...
+            #print("All were shown")
+            if now - self.cooldown >= self.blinkTime:
+                if now - self.activationTick >= self.blinkTime:
+                    self.disableAllChild()
+                key = gameInput.check()
+                self.checkInput(key)
+                if len(self.query) == self.guessIndex: # Wenn fertig geratem
+                    self.queryLength += 1
+                    self.setRandomQuery()
+                    self.blinkTime = self.blinkTime * 0.95
+                    self.round += 1
+                    self.index = 0
+                    self.guessIndex = 0
+                    self.activationTick = pygame.time.get_ticks()
+                    return
+                if self.query[self.guessIndex] == self.guess:
+                    self.guess = None
+                    print("True")
+                    self.guessIndex += 1
+                    self.score += 1
+                    self.cooldown = pygame.time.get_ticks()
+                elif self.guess:
+                    print("False")
+                    self.guess = None
+                    self.cooldown = pygame.time.get_ticks()
+                    # => Implement Loosing Logic
+
+                self.guess = None
+            else:
+                #print(self.guess)
+                pass
+                #print(self.query[self.guessIndex] == self.guess)
+            return
+        if now - self.activationTick >= self.blinkTime: # Wenn das akt. Feld lang genug geleuchtet hat...
+            # wechsle zum nächsten Feld
             current = self.query[self.index]
             print(current)
             self.disableAllChild()
-            if current == "red":
+            if current == "blue":
                 self.topLeft.activate()
-            elif current == "green":
+            elif current == "red":
                 self.topRight.activate()
-            elif current == "blue":
+            elif current == "green":
                 self.bottomLeft.activate()
-            elif current == "violet":
+            elif current == "yellow":
                 self.bottomRight.activate()
             self.index += 1
             self.activationTick = pygame.time.get_ticks()
@@ -94,20 +135,23 @@ class Field:
             
 
     def checkInput(self, input):
-        if input and len(self.query) == self.index:
+        if input:
             if input == "up":
-                self.index = 0
-                self.setRandomQuery(5)
+                self.guess = "blue"
+                self.topLeft.activate()
             elif input == "right":
+                self.guess = "red"
                 self.topRight.activate()
             elif input == "left":
+                self.guess = "green"
                 self.bottomLeft.activate()
             elif input == "down":
+                self.guess = "yellow"
                 self.bottomRight.activate()
 
-    def setRandomQuery(self, length):
+    def setRandomQuery(self):
         result = []
-        for i in range(length):
+        for i in range(self.queryLength):
             pool = list(self.colorsToKey.keys())
             if i > 0:
                 pool.remove(result[i - 1])
@@ -133,6 +177,16 @@ class Corner:
             # if now - self.activationTick >= self.blinkTime:
             #     self.active = False
 
+    def setColor(self, color):
+        if color == "red":
+            self.color = red
+        elif color == "blue":
+            self.color == blue
+        elif color == "green":
+            self.color = green
+        elif color == "yellow":
+            self.color == yellow
+
     def activate(self):
         self.active = True
         # self.activationTick = pygame.time.get_ticks()
@@ -145,6 +199,7 @@ class Corner:
 
 # Pygame Setup
 pygame.init()
+pygame.font.init()
 screen = pygame.display.set_mode((1000, 720));
 pygame.display.set_caption("Farbfelder")
 clock = pygame.time.Clock() 
@@ -153,7 +208,7 @@ dt = 0 # Für alles was sich bewegen würde
 smallBoxWidth = 45;
 smallBoxDistance = 250;
 
-gameInput = GameInput(False)
+gameInput = GameInput(True)
 user = User()
 
 # Erstellung der Grafikobjekte
@@ -176,10 +231,11 @@ while running:
 
     mainBox.draw()
 
-    key = gameInput.check()
-    if mainBox.checkInput(key):
-        user.scoreUp()
-        print(user.score)
+    
+
+    # if mainBox.checkInput(key):
+        # user.scoreUp()
+        # print(user.score)
         #mainBox.randomColor()
 
     pygame.display.flip()
