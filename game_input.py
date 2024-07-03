122d5f2984fa6c39ff11554e5819945d0755504f
Ilyas Kleine <77439904+ilyaskleine@users.noreply.github.com>
2024-07-03 13:36:43 +0200

    Jede 3. Messung

--- game_input.py
+++ game_input.py
@@ -22,6 +22,11 @@ class GameInput:
         self.meanDataR = []
         self.meanDataF = []
         self.meanDataB = []
+
+        self.DataL = []
+        self.DataR = []
+        self.DataF = []
+        self.DataB = []
     
     def get_pressed(self):
         return pygame.key.get_pressed()
@@ -55,30 +60,37 @@ class GameInput:
                     self.meanCount += 1
                 else:
                     self.meanCount = 0
-                    l = statistics.median(self.meanDataL)
-                    r = statistics.median(self.meanDataR) / 10
-                    f = statistics.median(self.meanDataF) / 10
-                    b = statistics.median(self.meanDataB) / 10
-                    if l < self.threshold:
-                        self.lockL = True
-                        return "left"
-                    elif self.lockL and l > self.releaseThreshold:
-                        self.lockL = False
-                    if r < self.threshold:
-                        self.lockR = True
-                        return "right"
-                    elif self.lockR and r > self.releaseThreshold:
-                        self.lockR = False
-                    if f < self.threshold:
-                        self.lockF = True
-                        return "up"
-                    elif self.lockF and f > self.releaseThreshold:
-                        self.lockF = False
-                    if b < self.threshold:
-                        self.lockB = True
-                        return "down"
-                    elif self.lockB and b > self.releaseThreshold:
-                        self.lockB = False
+                    if self.dataCount < 3:
+                        self.dataL.append(statistics.median(self.meanDataL))
+                        self.dataR.append(statistics.median(self.meanDataR) / 10)
+                        self.dataF.append(statistics.median(self.meanDataF) / 10)
+                        self.dataB.append(statistics.median(self.meanDataB) / 10)
+                    else:
+                        for l in self.dataL:
+                            if l < self.threshold:
+                                self.lockL = True
+                                return "left"
+                            elif self.lockL and l > self.releaseThreshold:
+                                self.lockL = False
+                        for r in self.dataR:
+                            if r < self.threshold:
+                                self.lockR = True
+                                return "right"
+                            elif self.lockR and r > self.releaseThreshold:
+                                self.lockR = False
+                        for f in self.dataF:
+                            if f < self.threshold:
+                                self.lockF = True
+                                return "up"
+                            elif self.lockF and f > self.releaseThreshold:
+                                self.lockF = False
+                        for b in self.dataB:
+                            if b < self.threshold:
+                                self.lockB = True
+                                return "down"
+                            elif self.lockB and b > self.releaseThreshold:
+                                self.lockB = False
+                    
 
         except json.decoder.JSONDecodeError:
             print("Decode-Error");
