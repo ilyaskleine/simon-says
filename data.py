65967bd6419baa487a8fb8634818b6bc0ddd305f
ilyaskleine <ilyaskleine07@gmail.com>
2026-01-28 00:50:33 +0100

    Init v2

--- data.py
+++
@@ -1,8 +0,0 @@
-
-class Data:
-    def __init__(self):
-        self.left = None
-        self.right = None
-        self.back = None
-        self.front = None
-
--- gamestate.py
+++
@@ -1,3 +0,0 @@
-class GameState:
-    def __init__(self):
-        self.activeField = None
